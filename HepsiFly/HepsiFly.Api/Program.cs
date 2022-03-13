29be8765551b97b2626716dc73b216dec0d82e6a
Samet Gunduz <sametgunduz@outlook.com>
2022-03-13 18:37:35 +0300

    added env params

--- HepsiFly/HepsiFly.Api/Program.cs
+++ HepsiFly/HepsiFly.Api/Program.cs
@@ -21,11 +21,9 @@ builder.Services.AddValidations();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app
     .UseGlobalExceptionMiddleware()
